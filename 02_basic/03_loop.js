6331ae8161ec5902c0d10dacba05368bda1c809a
pal6934 <140111425+pal6934@users.noreply.github.com>
2024-01-23 20:13:55 +0000

    map, filter and reduce loop

---
+++ 02_basic/03_loop.js
@@ -0,0 +1,48 @@
+// const num = [1,2,3,4,5,6,7,8,9];
+// const num1 = [
+//     {
+//         NameList : "Vijay",
+//         Id : 2
+//     },
+//     {
+//         NameList : "Rahul",
+//         Id : '2'
+//     },
+//     {
+//         NameList : "Ram",
+//         Id: 3
+//     },
+//     {
+//         NameList : "Sham",
+//         Id: 2
+//     }
+
+// ];
+
+// const numStore = [];
+
+// num.forEach((items) => {
+//     if(items > 5){
+//         numStore.push(items)
+//     }    
+// })
+// console.log(numStore)
+
+// const numArr = num1.filter( (items) => ((items.Id == 3)) )
+// console.log(numArr);
+
+// const numberL = [15,26,30,45,500];
+
+// const numAdd = numberL
+//                 .map( (num) => num -  (num * 50/100 ))
+
+// console.log(numAdd)
+
+const num = [1,2,3]
+
+const totalV = num.reduce( (arr, crr) => {
+    console.log(`arr : ${arr} and crr : ${crr}`)
+   return arr + crr
+},0)
+
+console.log(totalV)
